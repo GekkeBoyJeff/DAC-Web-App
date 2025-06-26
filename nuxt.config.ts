7904eccaada0543108effdf72eb7bda3644a25db
Jeffrey Ullers <jeffrey.ullers@hva.nl>
2025-06-26 13:10:27 +0200

    added options to config for supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,13 @@ export default defineNuxtConfig({
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
+    redirectOptions: {
+      login: '/login',
+      callback: '/confirm',
+      include: ['/dashboard', '/dashboard/**'],
+      exclude: [],
+      saveRedirectToCookie: false
+    }
   },
   app: {
     head: {

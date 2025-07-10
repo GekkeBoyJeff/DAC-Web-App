bdc37761a85308c5f34c690558edafc1e6a459b0
GekkeBoyJeff <jeffrey.ullers@hva.nl>
2025-07-10 06:29:45 +0200

    tried out glassmorphism with extra movement

--- components/appHeader.vue
+++ components/appHeader.vue
@@ -1,41 +1,45 @@
 <script lang="ts" setup>
-const colorMode = useColorMode()
+const header = ref<HTMLElement | null>(null)
+const nav = ref<HTMLElement | null>(null)
 
-// TODO: header pill maken on scroll
+const onScroll = () => {
+  if (header.value) {
+    if (window.scrollY > 1) {
+    	header.value.classList.add('header-scrolled')
+    } else {
+		header.value.classList.remove('header-scrolled')
+    }
+  }
+}
 
-const isDark = computed({
-	get() {
-		return colorMode.value === 'dark'
-	},
-	set(_isDark) {
-		colorMode.preference = _isDark ? 'dark' : 'light'
-	},
+onMounted(() => {
+  window.addEventListener('scroll', onScroll)
 })
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', onScroll)
+})
+
+
 </script>
 
 <template>
-	<header class="flex items-center justify-between p-4 top-0 left-0 z-10 w-full backdrop-blur-sm  absolute h-20">
-		<nav>
-			<ul class="flex gap-2">
-				<li>
-					<NuxtLink to="/">Home</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="/login">Login</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="https://discord.gg/dutchanimecommunity" class="p-2 border rounded-lg font-bold"
-						external>Join community</NuxtLink>
-				</li>
-			</ul>
-		</nav>
-		<ClientOnly v-if="!colorMode?.forced">
-			<UButton :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'" color="neutral" variant="ghost"
-				@click="isDark = !isDark" />
-
-			<template #fallback>
-				<div class="size-8" />
-			</template>
-		</ClientOnly>
-	</header>
+  <header ref="header" class="grid grid-cols-12 items-top top-4 left-0 z-10 w-full fixed h-20 bg-DacWhite/15 px-4">
+	<nav ref="nav" class="p-4 items-center translate-x-0 duration-500 md:col-span-4 flex">
+		<ul class="flex gap-6 text-DacBlack align-middle w-full">
+			<li>
+				<NuxtLink to="/">Home</NuxtLink>
+			</li>
+			<li>
+				<NuxtLink to="/login">Login</NuxtLink>
+			</li>
+			<li class="sr-only">
+				<NuxtLink to="https://discord.gg/dutchanimecommunity" class="p-2 border rounded-lg font-bold" external>Join community</NuxtLink>
+			</li>
+		</ul>
+	</nav>
+	<div class="p-2 border rounded-lg font-bold h-10 self-center md:col-span-2 lg:col-span-1 md:col-start-11 lg:col-start-12 text-center">
+		<NuxtLink to="https://discord.gg/dutchanimecommunity" external>Word lid!</NuxtLink>
+	</div>
+  </header>
 </template>
--- components/heroSlider.vue
+++ components/heroSlider.vue
@@ -4,7 +4,8 @@ const slides = [
     {
         name: 'Welkom bij Dutch Anime Community',
         description: 'De grootste anime community van Nederland',
-        image: 'https://picsum.photos/1920/1080',
+        // image: 'https://picsum.photos/1920/1080',
+        image: '',
         alt: 'Welkom bij Dutch Anime Community',
         link: '/about',
         linkText: 'Lees meer',
@@ -14,9 +15,9 @@ const slides = [
 </script>
 
 <template>
-    <div class="h-dvh w-dvw overflow-hidden relative">
+    <div class="h-dvh w-dvw overflow-hidden relative max-w-full">
         <div v-for="(slide, index) in slides" :key="index" class="relative grid h-full w-full grid-cols-12 grid-rows-8">
-            <img :src="slide.image" alt=slide.alt class="h-dvh w-dvw object-cover col-span-12" />
+            <img :src="slide.image" :alt=slide.alt class="h-dvh w-dvw object-cover col-span-12" />
             <div
                 class="flex flex-col text-white bg-black/50 p-2 rounded-md col-span-2 col-start-2 row-start-6 row-span-2">
                 <h2>{{ slide.name }}</h2>

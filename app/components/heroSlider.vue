fb492dda737dc7d850add87fccd56416c1957882
Jeffrey Ullers <jeffrey.ullers@hva.nl>
2025-09-05 01:46:35 +0200

    css header fix

--- app/components/heroSlider.vue
+++ app/components/heroSlider.vue
@@ -3,7 +3,7 @@
 const slides = [
     {
         name: 'Welkom bij Dutch Anime Community',
-        description: 'De grootste anime community van Nederland',
+        description: 'De grootste anime community van Nederland en België. Of je nu van manga, games, cosplay of de Japanse cultuur houdt: bij ons vind je altijd gelijkgestemden.',
         image: '/images/Spelletjesmiddag.jpg',
         alt: 'Welkom bij Dutch Anime Community',
         link: '/about',
@@ -20,18 +20,19 @@ const slides = [
             <NuxtImg :src="slide.image" :alt="slide.alt" :placeholder="[50, 25, 75, 5]"
                 class="bg-top absolute w-full h-full object-cover object-[50%_25%] brightness-75" loading="eager" />
             <div
-                class="text-DacWhite col-start-1 md:p-0 row-start-3 md:row-start-4 col-span-6 row-span-1 md:row-span-2 grid gap-2">
-                <h2 class="text-4xl font-bold">{{ slide.name }}</h2>
-                <p>{{ slide.description }}</p>
-                <nuxt-link :to="slide.link" class="bg-DacWhite p-4 text-DacBlack rounded-full w-fit">
+                class="hero-content text-realWhite col-start-1 md:p-0 row-start-4 md:row-start-4 col-span-6 row-span-1 md:row-span-2 grid gap-2">
+                <h2 class="hero-title text-4xl font-bold">{{ slide.name }}</h2>
+                <p class="hero-description">{{ slide.description }}</p>
+                <nuxt-link :to="slide.link"
+                    class="hero-cta flex items-center bg-primary py-2 px-4 text-DacBlack rounded-full w-fit">
                     {{ slide.linkText }}
-                    <span class="text-DacWhite bg-black p-2 rounded-[100%] ml-4 -mr-2">
+                    <span class="flex items-center text-DacWhite bg-black p-2 rounded-[100%] ml-4 -mr-2">
                         <UIcon name="i-lucide-arrow-up-right" class="size-5" />
                     </span>
                 </nuxt-link>
             </div>
             <div
-                class="col-span-6 -row-start-3 md:-row-start-2 md:col-start-6 md:p-0 col-start-1 md:-mx-8 md:mb-4 md:-mt-4">
+                class="stats col-span-6 -row-start-3 md:-row-start-2 md:col-start-6 md:p-0 col-start-1 md:-mx-8 md:mb-4 md:-mt-4">
                 <ul
                     class="flex flex-wrap gap-4 text-DacWhite h-auto -mt-4 sm:mt-0 md:h-full p-4 md:p-0 justify-around sm:justify-evenly sepia-50 items-center bg-DacWhite/10 backdrop-blur-md rounded-2xl">
                     <li class="flex flex-col items-center">
